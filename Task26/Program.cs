16e1f5f6079e0a8c09995f9347102006d449eb06
HotKiss <TanayKlim@mail.ru>
2023-10-05 12:47:06 +0400

    Seminar4

---
+++ Task26/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+int CountDigit(int num)
+{
+    int count = 0;
+    if (num == 0) return 1;
+    
+    while (num != 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Введите целое число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int countDigit = CountDigit(number);
+Console.WriteLine($"{number} -> {countDigit}");
