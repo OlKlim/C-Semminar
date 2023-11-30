03b91616c8f413dd14bdeea56eb55e9e71fad6b0
HotKiss <TanayKlim@mail.ru>
2023-11-30 06:43:39 +0400

    HomeWork 9

---
+++ Task66/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+Console.Write("Введите натуральное число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+if (numberM < 1 || numberN < 1)
+{
+    Console.WriteLine("Неккоректный ввод!");
+    return;
+}
+
+Console.WriteLine(Sum(numberM, numberN));
+
+int Sum(int numM, int numN) // 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
+{
+        if (numM == numN) return numM;
+        return numM + Sum(numM + 1, numN); // 1 2 3 4 5 6 7 8 9 10 11 12 13 14
+        // retuen 15 + 14 + 13...+1 = 120
+}
---
+++ Task68/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа n и m.
+// n = 2, m = 3 -> A(n,m) = 9
+// n = 3, m = 2 -> A(n,m) = 29
+
+Console.Write("Введите натуральное положительное число n: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное положительное число m: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+int ack = Ack(numberN, numberM);
+Console.WriteLine(ack);
+
+int Ack(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (m == 0) return Ack(n - 1, 1);
+    return Ack(n - 1, Ack(n, m - 1));
+}
